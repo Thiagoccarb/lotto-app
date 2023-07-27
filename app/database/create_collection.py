4e5b79ccc2273b9c3e64cc575cdaeadcca1a5535
Thiago Cabral Carboneri <thiago.carboneri@mindsdigital.net>
2023-07-26 22:06:44 -0300

    feat: Improves collection creation logic when app starting up

--- app/database/create_collection.py
+++ app/database/create_collection.py
@@ -3,9 +3,10 @@ from utils.logger import Logger
 
 async def check_collection_existence():
     logger = Logger()
-    existing_collections = await db.list_collection_names()
     logger.info('checking for existing collection')
-    target_collection_name = "lotto"
-    if target_collection_name not in existing_collections:
-        logger.info('checking for existing collection')
-        await db.create_collection(target_collection_name)
+    existing_collections = await db.list_collection_names()
+    logger.info(f'collections available: {", ".join(existing_collections)}')
+    collection_name = "lotofacil"
+    if collection_name not in existing_collections:
+        await db.create_collection(collection_name)
+        logger.info('colection {} has been created'.format(collection_name))
